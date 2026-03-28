ff4bc88708603974cc4906ef187e6f5ec8b3141a
Ramirezs873 <ramirezs@utas.edu.au>
2026-03-28 13:03:04 +1100

    28/03/26 1pm Update
    Updated gitignore.
    
    Added station_coords() to process.py. Easy way to gather coordinate data of seismic stations.

--- process.py
+++ process.py
@@ -6,7 +6,83 @@ from obspy import UTCDateTime as UTC
 from obspy import Stream
 from obspy import read
 from scipy import signal
+from obspy.clients.fdsn import Client
+from obspy import read_inventory
+from collections import defaultdict
+import obspy
 
+
+
+def station_coords(client, 
+                 network, 
+                 station,   
+                 user=None, 
+                 password=None, 
+                 config=None):
+
+    """
+    Gather seismic station coordinated from FDSN clients,
+    or from local files if they already exist, 
+    from one specified group of stations
+    and stores it as a dictionary.
+
+    Parameters:
+    ----------
+    client (str): 
+        FDSN client URL for group 1. e.g 'IRIS', "AUSPASS".
+    network (str): 
+        Network code for group 1. e.g 'IU', 'AU'.
+    station (str): 
+        Station code for group 1. e.g 'CASY', 'CWA90'. 
+    user (str):
+        Username to access the client.
+    password (str):
+        Password to acces the client.
+
+    Returns:
+    coord_dict (dict):
+        Dictionary containing station coordinate data.
+    """
+    
+    # Check if file already exists
+    # Config Support
+    base_path = Path(config["seismic_data_path"]) if config else Path(".")
+    base_path.mkdir(parents=True, exist_ok=True)
+
+    title = f'{network}_{station}'
+    
+    filename = f"station_data_{title}.xml"
+    file_path = base_path / filename
+
+
+    if file_path.exists():
+        print(f"Reading existing file: {file_path}")
+        station_data = read_inventory(str(file_path))
+
+    else:
+        print("File not found. Downloading data")
+
+        # Gather data from target stations
+        g1 = Client(base_url=client, 
+                    user=user, 
+                    password=password) # FDSN client 
+
+        station_data = g1.get_stations(network=network, 
+                                        station=station) # Gather station data
+
+        station_data.write(str(file_path), format="STATIONXML")
+        print(f"Saved to: {file_path}")
+
+    # Write to a dictionary
+    coord_dict = defaultdict(list) 
+
+    for net in station_data:
+        for sta in net:
+            key = f"{net.code}.{sta.code}"
+            coord_dict[key] = {"latitude": sta.latitude, 
+                                "longitude": sta.longitude}
+
+    return coord_dict   
 
 def preprocess(wave_dict,
                sensitivity = 2.994697576134245E8):
